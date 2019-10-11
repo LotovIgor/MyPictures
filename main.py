d6de13ebf46123d217bcb6ef0f6b04102ebb0de8
LotovIgor <ilotov@ngs.ru>
2019-10-11 22:40:19 +0700

    Add base 1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#test 1
+import turtle
+
+def square(x, y, size, color, angle):
+    # Пишет функцию, рисующую квадрат
+    pass
+
+def triangle(x, y, size, color, angle, type):
+    # Пишет функцию, рисующую треугольник
+    pass
+
+def rhomb(x, y, size, color, angle):
+    # Пишет функцию, рисующую ромб
+    pass
