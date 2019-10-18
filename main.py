ee92b8c276571a1e05fc0a4b4902bb5c12f497bc
MashaChep <56710297+MashaChep@users.noreply.github.com>
2019-10-18 11:39:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import turtle
-def sq(a, b, 
+def sq(a, x, y)
+  goto(x, y)
+  turtle.pendown()        
+  turtle.forward(a)
+  turtle.left(90)
+  turtle.forward(a)
+  turtle.left(90)
+  turtle.forward(a)
+  turtle.left(90)
+  turtle.forward(a)
+  turtle.left(90)
+  turtle.penup()  
