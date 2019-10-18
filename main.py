177d0f0cf0d7a319c26f25d62c8ec79b19dc7519
MashaChep <56710297+MashaChep@users.noreply.github.com>
2019-10-18 11:50:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,5 @@ def sq(a, x, y):
   turtle.left(90)
   turtle.penup()  
 
-def rec(a, b, x, y):
-  goto(x, y)
-  turtle.pendown() 
-  turtle.forward(a)
-  turtle.left(90)
-  turtle.forward(b)
-  turtle.left(90)
-  turtle.forward(a)
-  turtle.left(90)
-  turtle.forward(b)
-  turtle.left(90)
-  turtle.penup()  
+
   
