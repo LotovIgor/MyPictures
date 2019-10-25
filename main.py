ae2b381581ae7818df9f721107c44307635d9769
LotovIgor <ilotov@ngs.ru>
2019-10-25 09:18:03 +0800

    add square

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import turtle
-print("Masha is the best in the world!")
+
+def square(x, y, size, color):
+    # Пишет функцию, рисующую квадрат
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.penup()
+    pass
+
+def triangle(x, y, size, color, angle, type):
+    # Пишет функцию, рисующую треугольник
+    pass
+
+def rhomb(x, y, size, color, angle):
+    # Пишет функцию, рисующую ромб
+    pass
