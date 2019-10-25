112da31fd356190bf9d3a0e2945f3697579537f0
LotovIgor <ilotov@ngs.ru>
2019-10-25 22:45:37 +0700

    triangle + some comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-
+# !!!!!!!!После написания процедуры поворачиваем черепашку так, чтобы смотрела вправо, как в начале!!!!!
 
 def square(x, y, size, color, angle):
     # Маша пишет функцию, рисующую квадрат
@@ -21,12 +21,25 @@ def square(x, y, size, color, angle):
 def triangle(x, y, a, b, color, angle):
     # Игорь пишет функцию, рисующую треугольник
     # Треугольники только прямоугольные, вводим координаты острого угла при а
+    # Пример: triangle(0, 0, 200, 100, 'red', 120)
+    turtle.color(color)
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.left(angle)
+    turtle.forward(a)
+    turtle.left(90)
+    turtle.forward(b)
+    turtle.goto(x,y)
+    turtle.end_fill()
+    turtle.up()
     pass
 
 
 def rhomb(x, y, size, color, angle):
     # Нил пишет функцию, рисующую ромб
     pass
+
 
 # Далее под комментариями пишем каждый свою часть кода, рисующую у каждого свой рисунок
 # Маша рисует медведя
@@ -41,4 +54,5 @@ def rhomb(x, y, size, color, angle):
 
 # Нил рисует
 
+# Строка выхода из окна рисования по клику
 turtle.exitonclick()
