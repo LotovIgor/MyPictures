031b6f72527f353f3a0cd21365b2912913b91d57
LotovIgor <ilotov@ngs.ru>
2019-11-14 23:27:31 +0700

    snake

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import turtle
 import math
 from turtle import *
+turtle.tracer(0)
 # !!!!!!!!После написания процедуры поворачиваем черепашку так, чтобы смотрела вправо, как в начале!!!!!
-# Давайте рисовать в области 1220x410 (три столбца по две фигуры у каждого свой, интервалы 10 между областями)
+# Давайте рисовать в области 1220x810 (три столбца по две фигуры у каждого свой, интервалы 10 между областями)
 # Координаты 0,0 по середине
 # Игорь берет себе левый столбец
 
@@ -69,7 +70,11 @@ triangle(-485 - 100 * math.cos(math.radians(30)), 105, 100 * math.cos(math.radia
          100 * math.sin(math.radians(25)), 'green', 5)
 triangle(-485, 255, 100 * math.sin(math.radians(25)), 100 * math.cos(math.radians(25)), 'darkgreen', 85)
 # Игорь рисует змею
-
+triangle(-435, -205, 100/math.sqrt(2), 100/math.sqrt(2), 'forestgreen', 135)
+triangle(-485, -205, 100/math.sqrt(2), 100/math.sqrt(2), 'green', -45)
+triangle(-435, -205, 50, 50, 'darkgreen', 0)
+triangle(-335, -155, 50, 50, 'yellow', -90)
+triangle(-335, -155, 50, 50, 'gold', -45)
 # Нил рисует
 
 # Нил рисует
